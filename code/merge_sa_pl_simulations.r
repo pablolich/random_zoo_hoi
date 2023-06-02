97fe074b99dff7eb69d362cb6c45b2cb0f243ac0
pablolich <plechon@ucm.es>
2023-06-01 19:35:01 -0500

    merged sa data and pl data

--- code/merge_sa_pl_simulations.r
+++ code/merge_sa_pl_simulations.r
@@ -4,7 +4,7 @@
 for (n in c(1,2,3,4,5,6,7,8)){
   for (d in c(2,3,4,5,6,7)){
     f1 <-  paste0("n_", n, "_d_", d, ".csv")
-    path = "../data/merged_files/sa_data/"
+    path = "../data/merged_files/sa_data2/"
     if (file.exists(paste0(path,f1))){
       dt <- read.table(paste0(path,f1), sep = ",", header = T) #load r
       colnames(dt) = c("n", "d", "nsol", "npos")
